a7a885cb34ba272153c07c791e5d7d65f8eb9145
JUNHEE LEE <jun48470@gmail.com>
2022-10-05 17:05:55 +0900

    feat : firebase 장비 등록 및 상태 변경 기능 추가

--- Server/test_server/database/firebase.py
+++ Server/test_server/database/firebase.py
@@ -52,10 +52,48 @@ class FireBase():
 
 
     @staticmethod
-    def get_location(user_location_data:object) -> dict:
+    def get_location(user_location_data:object) -> None:
         """ 위치 가져오기 """
         try:
             FireBase.firebase_conn()
         except Exception:
             pass
+    
+    @staticmethod 
+    def set_device_name(user_device_data:object) -> None:
+        """ 장치 등록 """
+        try:
+            FireBase.firebase_conn()
+        except Exception:
+            pass
+    
+        ref = db.reference(f'/users/{user_device_data.user_id}/')
+        device_ref = ref.child(f'Device')
         
+        new_device_ref = device_ref.push() 
+        new_device_ref.set({
+                    'room' : f'{user_device_data.device_name}', 
+                    'status': f'{user_device_data.device_status}'        
+                })
+        
+        
+    @staticmethod 
+    def set_device_status(user_device_data:object) -> None:
+        """ 장치 등록 """
+        try:
+            FireBase.firebase_conn()
+        except Exception:
+            pass
+    
+        ref = db.reference(f'/users/{user_device_data.user_id}/')
+        device_ref = ref.child(f'Device').get()
+        
+        for key in device_ref.keys():
+            if device_ref[key]['room'] == user_device_data.device_name:
+                ref = db.reference(f'/users/{user_device_data.user_id}/Device')
+                ref = ref.child(f'{key}')
+                ref.update({
+                    "room": f"{user_device_data.device_name}",
+                    "status": f"{user_device_data.device_status}"
+                })
+            
