47913325d880382ea32226d500c69d70a61cf74b
JUNHEE LEE <jun48470@gmail.com>
2022-10-05 16:58:27 +0900

    feat : 블루투스 연결 기능 추가

--- app/SilverCare/app/src/main/java/com/remote/silvercare/ui/device/DeviceFragment.java
+++ app/SilverCare/app/src/main/java/com/remote/silvercare/ui/device/DeviceFragment.java
@@ -1,21 +1,65 @@
 package com.remote.silvercare.ui.device;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.remote.silvercare.databinding.FragmentDeviceBinding;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
 public class DeviceFragment extends Fragment {
 
     private FragmentDeviceBinding binding;
 
+    BluetoothSocket btSocket = null;
+    BluetoothAdapter btAdapter;
+    private final static int REQUEST_ENABLE_BT = 1;
+    ConnectedThread connectedThread;
+
+    UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
+
+
+    TextView textStatus;
+    Button btnPared, btnSearch;
+    ListView listView;
+
+    Set<BluetoothDevice> pairedDevices;
+    ArrayAdapter<String> btArrayAdapter;
+    ArrayList<String> deviceAddressArray;
+
+    ArrayList<String> listDeivceName = new ArrayList<String>();
+
+    @SuppressLint("HandlerLeak")
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         DeviceViewModel deviceViewModel =
@@ -24,14 +68,208 @@ public class DeviceFragment extends Fragment {
         binding = FragmentDeviceBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDevice;
-        deviceViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        String[] permission_list = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+
+        ActivityCompat.requestPermissions(getActivity(), permission_list, 1);
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        textStatus = binding.textStatus;
+        btnPared = binding.btnPared;
+        btnSearch = binding.btnSearch;
+        listView = binding.listview;
+
+        btArrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1);
+        deviceAddressArray = new ArrayList<>();
+        listView.setAdapter(btArrayAdapter);
+
+        btnPared.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btArrayAdapter.clear();
+                if (deviceAddressArray != null && !deviceAddressArray.isEmpty()) {
+                    deviceAddressArray.clear();
+                }
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    GetBlutoothPermission();
+                    return;
+                }
+                pairedDevices = btAdapter.getBondedDevices();
+                if (pairedDevices.size() > 0) {
+                    // There are paired devices. Get the name and address of each paired device.
+                    for (BluetoothDevice device : pairedDevices) {
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress(); // MAC address
+                        btArrayAdapter.add(deviceName);
+                        deviceAddressArray.add(deviceHardwareAddress);
+                    }
+                }
+            }
+        });
+
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    GetBlutoothPermission();
+                    return;
+                }
+                if (btAdapter.isDiscovering()) {
+                    btAdapter.cancelDiscovery();
+                } else {
+                    if (btAdapter.isEnabled()) {
+                        btAdapter.startDiscovery();
+                        btArrayAdapter.clear();
+                        if (deviceAddressArray != null && !deviceAddressArray.isEmpty()) {
+                            deviceAddressArray.clear();
+                            listDeivceName.clear();
+                        }
+                        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                        getActivity().registerReceiver(receiver, filter);
+                    } else {
+                        Toast.makeText(getActivity(), "bluetooth not on", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        listView.setOnItemClickListener(new ItemClickListener());
+
+
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    GetBlutoothPermission();
+                    return;
+                }
+
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+
+                if(listDeivceName.contains(deviceName) == false && deviceName != null)
+                {
+                        listDeivceName.add(deviceName);
+                        btArrayAdapter.add(deviceName);
+                        deviceAddressArray.add(deviceHardwareAddress);
+                        btArrayAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+    };
+
+    public void GetBlutoothPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            requestPermissions(
+                    new String[]{
+                            Manifest.permission.BLUETOOTH,
+                            Manifest.permission.BLUETOOTH_SCAN,
+                            Manifest.permission.BLUETOOTH_ADVERTISE,
+                            Manifest.permission.BLUETOOTH_CONNECT
+
+
+                    },
+                    1);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(
+                    new String[]{
+                            Manifest.permission.BLUETOOTH
+
+                    },
+                    1);
+        }
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        try{
+            getActivity().unregisterReceiver(receiver);
+        }catch (IllegalArgumentException e){
+        }catch (Exception e){
+        }finally {
+        }
+    }
+
+    public class ItemClickListener implements AdapterView.OnItemClickListener {
+
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(getActivity(), btArrayAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+
+            textStatus.setText("try...");
+
+            final String name = btArrayAdapter.getItem(position); // get name
+            final String address = deviceAddressArray.get(position); // get address
+            boolean flag = true;
+
+            BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+            // create & connect socket
+            try {
+                btSocket = createBluetoothSocket(device);
+                btSocket.connect();
+            } catch (IOException e) {
+                flag = false;
+                textStatus.setText("connection failed!");
+                e.printStackTrace();
+            }
+            if (flag) {
+                textStatus.setText("connected to " + name);
+                connectedThread = new ConnectedThread(btSocket);
+                connectedThread.start();
+            }
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        try {
+            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
+            return (BluetoothSocket) m.invoke(device, BT_MODULE_UUID);
+        } catch (Exception e) {
+            Log.e("createBluetoothSocket", "Could not create Insecure RFComm Connection", e);
+        }
+
+        return device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
+    }
+
 }
--- app/SilverCare/app/src/main/java/com/remote/silvercare/ui/device/DeviceViewModel.java
+++ app/SilverCare/app/src/main/java/com/remote/silvercare/ui/device/DeviceViewModel.java
@@ -5,15 +5,17 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class DeviceViewModel extends ViewModel {
-
-    private final MutableLiveData<String> mText;
-
-    public DeviceViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
+//
+//    private final MutableLiveData<String> mText;
+//
+//    public DeviceViewModel() {
+//        mText = new MutableLiveData<>();
+////        mText.setValue("This is gallery fragment");
+//
+//
+//    }
+//
+//    public LiveData<String> getText() {
+//        return mText;
+//    }
 }
