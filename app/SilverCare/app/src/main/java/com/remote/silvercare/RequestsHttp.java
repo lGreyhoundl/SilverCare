c6d982cc3fa685ef2c8500e703cfb9be68f5f9f2
JUNHEE LEE <jun48470@gmail.com>
2022-10-05 16:52:47 +0900

    refact : http request method 변경

--- app/SilverCare/app/src/main/java/com/remote/silvercare/RequestsHttp.java
+++ app/SilverCare/app/src/main/java/com/remote/silvercare/RequestsHttp.java
@@ -12,14 +12,16 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RequestsHttp {
+    JSONObject jsonObject;
     @RequiresApi(api = Build.VERSION_CODES.N)
-    public String Requests(URL url, JSONObject userParam) {
+    public JSONObject Requests(URL url, JSONObject userParam) {
         String Response = null;
         try{
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -30,25 +32,25 @@ public class RequestsHttp {
             conn.setDoInput(true);
 
             DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-            //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
-            Log.i("JSON", userParam.toString());
             os.writeBytes(userParam.toString());
+//            os.writeBytes(URLEncoder.encode(userParam.toString(), "UTF-8"));
+            Log.i("JSON", userParam.toString());
 
             os.flush();
             os.close();
             BufferedReader br = null;
 
             if (100 <= conn.getResponseCode() && conn.getResponseCode() <= 399) {
-                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
             } else {
-                br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                br = new BufferedReader(new InputStreamReader(conn.getErrorStream(),"UTF-8"));
             }
 
             String responseData = br.lines().collect(Collectors.joining());
             conn.disconnect();
 
-            JSONObject jsonObject = new JSONObject(responseData);
-            Response = jsonObject.getString("error");
+            jsonObject = new JSONObject(responseData);
+//            Response = jsonObject.getString("error");
             Log.i("STATUS", String.valueOf(conn.getResponseCode()));
             Log.i("ServerResponse", responseData);
             Log.i("MSG" , conn.getResponseMessage());
@@ -56,52 +58,6 @@ public class RequestsHttp {
         }catch (Exception e) {
             e.printStackTrace();
         }
-        return Response;
+        return jsonObject;
     }
-
-
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public HashMap<String, String> RequestsLocation(URL url, JSONObject userParam) {
-        HashMap<String, String> location = new HashMap<>();
-        try{
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-            conn.setRequestProperty("Accept","application/json");
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-
-            DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-            //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
-            Log.i("JSON", userParam.toString());
-            os.writeBytes(userParam.toString());
-
-            os.flush();
-            os.close();
-            BufferedReader br = null;
-
-            if (100 <= conn.getResponseCode() && conn.getResponseCode() <= 399) {
-                br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            } else {
-                br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-            }
-
-            String responseData = br.lines().collect(Collectors.joining());
-            conn.disconnect();
-
-            JSONObject jsonObject = new JSONObject(responseData);
-            location.put("latitude", jsonObject.getString("latitude"));
-            location.put("longitude", jsonObject.getString("longitude"));
-
-            Log.i("STATUS", String.valueOf(conn.getResponseCode()));
-            Log.i("ServerResponse", responseData);
-            Log.i("MSG" , conn.getResponseMessage());
-
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-        return location;
-    }
-
 }
